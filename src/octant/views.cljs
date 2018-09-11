ea81c2acf4eb059f6e7dcfcbb5f84e2aa93cd78f
dantheobserver <daniel.alan.p@gmail.com>
2018-09-10 19:55:56 -0700

    Todo task interaction.

--- src/octant/views.cljs
+++ src/octant/views.cljs
@@ -1,11 +1,12 @@
 (ns octant.views
-  (:require [reagent.core :as r :refer [atom]]))
+  (:require [reagent.core :as r :refer [atom]]
+            [clojure.string :as string]))
 
 (def app-state (atom {:todos {0 {:id 0 :task "Take out the trash in the morning" :active true}
                               1 {:id 1 :task "visit the Museum of Modern art"}
                               2 {:id 2 :task "Take the train to post office"}
                               3 {:id 3 :task "Research lessons on Reagent"}
-                              4 {:id 4 :task "get food at River market for dinner"}}}))
+                              4 {:id 4 :task "get food at River market for   dinner"}}}))
 
 ;; State selectors
 (defn todos [] (:todos @app-state))
@@ -43,14 +44,47 @@
        [:h2.subtitle.is-centered (:task task)]]
       [:h1.title "No current tasks"])]])
 
+(defn hover-text [text]
+  (let [hover-idx (atom nil)
+        sel-idxs (atom #{})
+        set-hover (fn [i]
+                    (when-not (get @sel-idxs i)
+                      (reset! hover-idx i)))
+        toggle-select (fn [i]
+                        (if (get @sel-idxs i)
+                          (do
+                            (swap! sel-idxs disj i)
+                            (set-hover i))
+                          (do
+                            (swap! sel-idxs conj i)
+                            (set-hover nil))))]
+    (fn [text]
+      [:span
+       (doall
+        (map-indexed (fn [i word]
+                       ^{:key i}
+                       [:span {:on-mouse-over #(set-hover i)
+                               :on-click #(do
+                                            (toggle-select i)
+                                            (.stopPropagation %))
+                               :class (when (get @sel-idxs i) "tag is-info")}
+                        [:span
+                         (when (= @hover-idx i) {:class "tag is-primary"})
+                         word]
+                        [:span " "]])
+                     (string/split text #"\s+")))])))
+
+(defn list-item [{:keys [task id active]}]
+  [:div.box (merge
+             {:on-click #(set-active-task! id)}
+             (when active {:class "has-background-light"}))
+   [:section.section
+    [hover-text task]]])
+
 (defn app []
   [:div
    [banner]
    [:section.section
-    (for [{:keys [task id active]} (todos-seq)]
-      ^{:key id}
-      [:div.box (merge
-                 {:on-click #(set-active-task! id)}
-                 (when active {:class "has-background-light"}))
-       [:section.section
-        [:span task]]])]])
+    (for [task (todos-seq)]
+      ^{:key (:id task)}
+      [list-item task])]])
